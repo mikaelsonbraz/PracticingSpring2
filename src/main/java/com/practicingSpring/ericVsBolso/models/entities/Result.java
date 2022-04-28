642a2c1854d91188186d7010883df08ee0b748cb
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-04-28 11:07:18 -0300

    Entities and Enums created

---
+++ src/main/java/com/practicingSpring/ericVsBolso/models/entities/Result.java
@@ -0,0 +1,46 @@
+package com.practicingSpring.ericVsBolso.models.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Result {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Integer idResult;
+
+    @Column
+    private Integer score;
+
+    @JoinColumn
+    @OneToOne
+    private User user;
+
+    @OneToMany
+    private List<Question> questionList;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Result result = (Result) o;
+        return Objects.equals(idResult, result.idResult) && Objects.equals(score, result.score) && Objects.equals(user, result.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idResult, score, user);
+    }
+}
