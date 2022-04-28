d2acbae7b67cbacd83c49b538953e21a3e62b4de
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-04-28 10:38:16 -0300

    first commit

---
+++ src/main/java/com/practicingSpring/ericVsBolso/EricVsBolsoApplication.java
@@ -0,0 +1,13 @@
+package com.practicingSpring.ericVsBolso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EricVsBolsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EricVsBolsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practicingSpring/ericVsBolso/EricVsBolsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicingSpring.ericVsBolso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EricVsBolsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
